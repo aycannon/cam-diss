edd65f68ccb4ee3774aab5ae5168801cc22546f7
Archie Cannon <acannon@Archies-MacBook-Pro-2.local>
2025-07-05 20:00:00 +0100

    on other laptop - changing preamble

--- preamble.R
+++ preamble.R
@@ -2,13 +2,14 @@
 
 
 
-library(highcharter)
+# library(highcharter)
 library(data.table)
 library(magrittr)
 # install.packages("progress")
 library(progress)
-library(R.matlab)
-library(reshape2)
+#library(R.matlab)
+# install.packages("reshape2")
+# library(reshape2)
 library(xts)
 library(mice)
 # install.packages("aod")
@@ -28,28 +29,29 @@ library(readxl)
 library(tidyverse)
 library(lubridate)
 library(sandwich)
-library(quantreg)
+# library(quantreg)
 library(car)
-library(quantregForest)
+# install.packages("quantregForest")
+# library(quantregForest)
 library(RcppRoll)
-install.packages('latex2exp')
-library(latex2exp)
+# install.packages('latex2exp')
+# library(latex2exp)
 
 
-library(VIM)
+# library(VIM)
 
 library(timetk)
 library(tibbletime)
 library(scales)
 
 library(TTR) # HLOC vol estimation
-library(highfrequency) # HAR vol
-library(ROI)
+# library(highfrequency) # HAR vol
+# library(ROI)
 # library(ROI.plugin.glpk)
 
-library(tidyquant)
+# library(tidyquant)
 library(quantmod)
-library(PortfolioAnalytics)
+# library(PortfolioAnalytics)
 library(MarkowitzR)
 library(quadprog)
 library(XICOR)
@@ -57,27 +59,27 @@ library(SharpeR)
 
 library(PeerPerformance)
 
-library(nlshrink) # covariance estimation - shrinkage
+#library(nlshrink) # covariance estimation - shrinkage
 # install.packages("bidask")
-library(bidask)
-library(selectiveInference)
+#library(bidask)
+#library(selectiveInference)
 library(glmnet) # cv
-library(gcdnet) # elastic nets
-library(msaenet) # elastic nets
-library(kernlab) # kernel 
+#library(gcdnet) # elastic nets
+#library(msaenet) # elastic nets
+# library(kernlab) # kernel 
 
 # Class 5
 library(grf)
 library(ranger)
-library(randomForest)
+# library(randomForest)
 library(caret)
-library(xgboost)
+#library(xgboost)
 library(neuralnet)
 library(doParallel)
 
 # class 6
-library(pls)
-library(hdm)
+#library(pls)
+#library(hdm)
 
 # Class 7
 # install.packages("remotes")
